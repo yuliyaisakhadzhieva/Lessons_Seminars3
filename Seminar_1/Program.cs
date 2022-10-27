76cfef36ffc3c2a782eecd01d83bb3d254d7f968
Yuliya <yuliya_saveleva@list.ru>
2022-10-27 22:41:31 +0500

    Add info

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите натуральное число ");
+int x=Convert.ToInt32 ( Console.ReadLine());
+int y = x*x;
+Console.WriteLine("Вот посчитали квадрат введеного вами числа и это цифра - "+y);
+if (x==8)
+{
+    Console.WriteLine("Вы ввели восемь");
+}
+else Console.WriteLine("Вы ввели число отличное от 8");
+int i = 0;
+while (i<x)
+{
+    Console.WriteLine($"счетчик цикла равен {i}");
+    i=i+1;
+}
