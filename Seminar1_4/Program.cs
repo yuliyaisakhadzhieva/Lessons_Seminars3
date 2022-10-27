8ae6ced5cbd4b5e8b0af9e2f204527f0e7fb6f0b
Yuliya <yuliya_saveleva@list.ru>
2022-10-27 23:19:33 +0500

    Task1_1modern

---
+++ Seminar1_4/Program.cs
@@ -0,0 +1,15 @@
+﻿try
+{
+    Console.WriteLine("Введите натуральное число ");
+    int x=Convert.ToInt32 ( Console.ReadLine());
+
+    Console.WriteLine("Введите натуральное число ");
+    int y=Convert.ToInt32 ( Console.ReadLine());
+    if (x*x==y) Console.WriteLine ("Второе число является квадратом первого");
+    else if (y*y==x) Console.WriteLine ("Первое число является квадратом второго");
+    else Console.WriteLine ("No");
+}
+catch
+{
+Console.WriteLine("Вы ввели некорректные данные");
+}
