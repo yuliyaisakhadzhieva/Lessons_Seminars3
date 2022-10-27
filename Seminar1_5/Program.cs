bb095dc9783de4f60c07bb013fe54b73b2c794d8
Yuliya <yuliya_saveleva@list.ru>
2022-10-27 23:50:42 +0500

    Task1_3

---
+++ Seminar1_5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите натуральное число ");
+int x=Convert.ToInt32 ( Console.ReadLine());
+int count = -x;
+while (count <- x);
+{
+    Console.Write (count+ " ");
+    count = count + 1;
+}
