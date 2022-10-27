1427fc97e985661094feb4781b740df26ffbdcc1
Yuliya <yuliya_saveleva@list.ru>
2022-10-27 23:12:07 +0500

    Task1_2

---
+++ Seminar1_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число дня недели");
+int x=Convert.ToInt32 ( Console.ReadLine());
+
+if (x == 1) Console.WriteLine ("Понедельник");
+else if (x == 2) Console.WriteLine ("Вторник");
+else if (x == 3) Console.WriteLine ("Среда");
+else if (x == 4) Console.WriteLine ("Четверг");
+else if (x == 5) Console.WriteLine ("Пятница");
+else if (x == 6) Console.WriteLine ("Суббота");
+else if (x == 7) Console.WriteLine ("Воскресень;е");
+else Console.WriteLine ("Вы ввели не то число");
