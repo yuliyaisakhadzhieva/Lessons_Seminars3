8b1f3e0bee5fde14714a69c381ffa33745a7ed39
Yuliya <yuliya_saveleva@list.ru>
2022-10-27 22:57:35 +0500

    Task1

---
+++ Seminar1_2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите натуральное число ");
+int x=Convert.ToInt32 ( Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число ");
+int y=Convert.ToInt32 ( Console.ReadLine());
+if (x*x==y) Console.WriteLine ("Yes");
+else Console.WriteLine ("No");
+if (y*y==x) Console.WriteLine ("Yes");
+else Console.WriteLine ("No");
