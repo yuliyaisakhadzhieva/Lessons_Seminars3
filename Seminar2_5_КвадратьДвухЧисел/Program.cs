e4e5d4c746ea1d9377fd6dcea51bf81d90bd7517
Yuliya <yuliya_saveleva@list.ru>
2022-10-29 15:53:03 +0500

    Add new folders Seminars

---
+++ Seminar2_5_КвадратьДвухЧисел/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход два числа
+// и проверяет, является ли одно число квадратом другого.  
+// 5, 25  ->  да -4, 16  ->  да 25, 5  ->  да 8,9  ->  нет
+
+Console.WriteLine("Введите натуральное число ");
+int x=Convert.ToInt32 ( Console.ReadLine());
+Console.WriteLine("Введите натуральное число ");
+int y=Convert.ToInt32 ( Console.ReadLine());
+if (x*x == y ) Console.Write("Да");
+else if ( y*y == x) Console.Write("Да");
+else Console.Write("Нет");
