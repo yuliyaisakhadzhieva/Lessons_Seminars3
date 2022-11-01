cb7275f78cebfe3b897c013ee7e43587d377fcb1
Yuliya <yuliya_saveleva@list.ru>
2022-11-01 17:53:56 +0500

    Add new folders

---
+++ Example012_Виды_функций/Program.cs
@@ -0,0 +1,70 @@
+﻿// Вид 1 - ничего не принимают, ничего не возвращают
+
+// void Method1()
+// {
+//     Console.WriteLine ("Автор ...");
+// };
+// Method1();
+    
+
+// Вид 2 - что-то принимают, ничего не возвращают
+
+// void Method2 (string msg)
+// {
+//     Console.WriteLine (msg);
+// }
+// Method2 ("Текст сообщения");
+
+
+// void Method2 (string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine (msg);
+//         i++;
+//     }
+// }
+// // Method2 (msg: "Текст", count: 4);    ИЛИ
+// Method2 (count: 4, msg: "Новый текст");
+
+
+// Вид 3 - ничего не принимают, что-то возвращают
+
+// int Method3 ()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method3 ();
+// Console.WriteLine (year);
+
+
+// Вид 4 - что-то принимают, что-то возвращают
+
+// string Method4 (int count, string c)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while (i < count)
+//     {
+//         result = result + c;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4 (10, "abc");
+// Console.WriteLine (res);
+
+
+string Method4 (int count, string text)
+{
+    string result = string.Empty;
+    for( int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4 (5,"abc");
+Console.WriteLine (res);
+
---
+++ Example014_СортировкаМассива/Program.cs
@@ -0,0 +1,28 @@
+﻿int [] arr = {1, 5, 7, 3, 4, 8, 9, 1, 1};
+void PrintArray (int [] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write ($"{array [i]}");
+    }
+    Console.WriteLine ();
+}
+void SelectionSort (int [] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array [j] < array [minPosition]) minPosition = j;
+        }
+        int temprorary = array [i];
+        array [i] = array [minPosition];
+        array [minPosition] = temprorary;
+    }
+}
+PrintArray (arr);
+SelectionSort (arr);
+
+PrintArray (arr);
