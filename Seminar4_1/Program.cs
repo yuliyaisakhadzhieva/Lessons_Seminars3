e9ec9ad1a6ce7aa04e2005cece6186477b09a18b
Yuliya <yuliya_saveleva@list.ru>
2022-11-02 13:41:20 +0500

    Add new info in Seminar4

--- Seminar4_1/Program.cs
+++ Seminar4_1/Program.cs
@@ -1,4 +1,35 @@
-﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) 
+// и выдаёт сумму чисел от 1 до А. 7 -> 28 4 -> 10 8 -> 36
+
+// Console.WriteLine("Введите целое число"); 
+// int n = Convert.ToInt32(Console.ReadLine());
+// int sum=0;
+// for (int i =1; i<=n;i++) sum=sum+i;
+// Console.WriteLine($"Сумма всех чисел от 1 до {n} равна {sum}");  
+// void SumNumbers(int x) 
+// {     
+//     int sum=0;     
+//     for (int i =1; i<=x;i++)          
+//         sum=sum+i;     
+//         Console.WriteLine($"Сумма всех чисел от 1 до {x} равна {sum}");
+// }  
+// int SumNumbers2(int x)
+// {     
+//     int sum=0;     
+//     for (int i =1; i<=x;i++)          
+//     sum=sum+i;     
+//     return sum; 
+// }   
+// Console.WriteLine("Введите целое число"); 
+// int n = Convert.ToInt32(Console.ReadLine()); 
+// SumNumbers(n); 
+// Console.WriteLine(SumNumbers2(n));
+
+
+
+
+
+// Задача 26: Напишите программу, которая принимает на вход число и выдаёт
 // количество цифр в числе. 456 -> 3 78 -> 2 89126 -> 5
 
 // Console.WriteLine("Введите целое число");
