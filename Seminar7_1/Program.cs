edffd37a089a8607f7320d001168a330ce6cf213
Yuliya <yuliya_saveleva@list.ru>
2022-11-08 00:01:26 +0500

    Add Seminar7

---
+++ Seminar7_1/Program.cs
@@ -0,0 +1,263 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+// Console.WriteLine("Введите количество строк");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int[,] FillArray(int m, int n)
+// {
+//     int[,] array = new int[m,n];
+//     for (int i=0;i<array.GetLength(0);i++)
+//     {
+//         for (int j=0;j<array.GetLength(1);j++)
+//             array[i,j]= new Random().Next(-30,31);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[,] image)
+// {
+//     for (int i = 0; i < image.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < image.GetLength(1); j++)
+//         {
+//             Console.Write($"{image[i, j]} \t");
+//             // if (image[i, j]<10 && image[i, j]>=0) Console.Write($"{image[i, j]} \t");
+//             // else if (image[i, j]<-9) Console.Write($"{image[i, j]} ");
+//             // else Console.Write($"{image[i, j]}  ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int[,] mas=FillArray(m,n);
+// PrintArray(mas);
+
+// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится
+// по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+// Console.WriteLine("Введите количество строк");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int[,] FillArray(int m, int n)
+// {
+//     int[,] array = new int[m, n];
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             array[i, j] = i + j;
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[,] image)
+// {
+//     for (int i = 0; i < image.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < image.GetLength(1); j++)
+//         {
+//             Console.Write($"{image[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int[,] mas = FillArray(m, n);
+// PrintArray(mas);
+
+
+// Задача HARD SORT.
+// Задайте двумерный массив из целых чисел. Количество строк и столбцов задается с клавиатуры. 
+// Отсортировать элементы по возрастанию слева направо и сверху вниз.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 10 3
+// После сортировки
+// 1 2 3 4
+// 5 7 9 10
+
+
+// Console.WriteLine("Введите количество строк");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int[,] FillArray(int m, int n)
+// {
+//     int[,] array = new int[m, n];
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             array[i, j] = new Random().Next(1, 10);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[,] image)
+// {
+//     for (int i = 0; i < image.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < image.GetLength(1); j++)
+//         {
+//             Console.Write($"{image[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int[,] mas = FillArray(m, n);
+// PrintArray(mas);
+
+// int[] NewArray = new int[m * n];
+// int k = 0;
+// for (int i = 0; i < mas.GetLength(0); i++)
+// {
+//     for (int j = 0; j < mas.GetLength(1); j++)
+//     {
+//         NewArray[k] = mas[i, j];
+//         k++;
+//     }
+// }
+
+// void SelectionSort (int [] NewArray [k])
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array [j] < array [minPosition]) minPosition = j;
+//         }
+//         int temprorary = array [i];
+//         array [i] = array [minPosition];
+//         array [minPosition] = temprorary;
+//     }
+// }
+// PrintArray (arr);
+// SelectionSort (arr);
+
+// PrintArray (arr);
+
+// ДОДЕЛАТЬ
+
+
+// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса нечётные, 
+// и замените эти элементы на их квадраты.
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Новый массив будет выглядеть 
+// вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+// Console.WriteLine("Введите количество строк");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int[,] FillArray(int m, int n)
+// {
+//     int[,] array = new int[m,n];
+//     for (int i=0;i<array.GetLength(0);i++)
+//     {
+//         for (int j=0;j<array.GetLength(1);j++)
+//             array[i,j]= new Random().Next(0,10);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[,] image)
+// {
+//     for (int i = 0; i < image.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < image.GetLength(1); j++)
+//         {
+//             Console.Write($"{image[i, j]} \t");
+//                   }
+//         Console.WriteLine();
+//     }
+// }
+
+// int[,] mas=FillArray(m,n);
+// PrintArray(mas);
+// Console.WriteLine();
+
+// int[,] SQArray(int [,] array)
+// {
+//         for (int i=1;i<array.GetLength(0);i=i+2)
+//     {
+//         for (int j=1;j<array.GetLength(1);j=j+2)
+//             array[i,j]= array[i,j]* array[i,j];
+//     }
+//     return array;
+// }
+// int[,] temp = SQArray (mas);
+// PrintArray (temp);
+
+
+// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся 
+// на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+Console.WriteLine("Введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] FillArray(int m, int n)
+{
+    int[,] array = new int[m,n];
+    for (int i=0;i<array.GetLength(0);i++)
+    {
+        for (int j=0;j<array.GetLength(1);j++)
+            array[i,j]= new Random().Next(0,10);
+    }
+    return array;
+}
+
+void PrintArray(int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            Console.Write($"{image[i, j]} \t");
+                  }
+        Console.WriteLine();
+    }
+}
+
+int[,] mas=FillArray(m,n);
+PrintArray(mas);
+Console.WriteLine();
+
+int SumArray(int [,] array)
+{
+    int sum = 0;
+        for (int i=0;i<array.GetLength(0);i++)
+    {
+        for (int j=0;j<array.GetLength(1);j++)
+           if (i==j) sum = sum + array [i,j];
+    }
+    return sum;
+}
+Console.WriteLine ($"Сумма чисел главной диагонали равна: "+ SumArray(mas));
